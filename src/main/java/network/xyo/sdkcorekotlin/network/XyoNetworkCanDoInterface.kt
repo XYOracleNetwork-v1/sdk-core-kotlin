ee0110d216dcdec5b274ca978ed37165d42abd2d
Carter Harrison <carterjharrison@gmail.com>
2018-09-05 17:28:59 -0700

    big fixes and network interface changes

---
+++ src/main/java/network/xyo/sdkcorekotlin/network/XyoNetworkCanDoInterface.kt
@@ -0,0 +1,4 @@
+package network.xyo.sdkcorekotlin.network
+
+interface XyoNetworkCanDoInterface {
+}
--- src/main/java/network/xyo/sdkcorekotlin/network/XyoNetworkReceiverListenerInterface.kt
+++
@@ -1,6 +0,0 @@
-package network.xyo.sdkcorekotlin.network
-
-interface XyoNetworkReceiverListenerInterface {
-    fun onConnection(peerConnection : XyoNetworkConnection, peer : XyoNetworkPeer, requestData : ByteArray)
-    fun onNewDeviceSeen()
-}
