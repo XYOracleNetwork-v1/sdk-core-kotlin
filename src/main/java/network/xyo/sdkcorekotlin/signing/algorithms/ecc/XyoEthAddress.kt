316524afdbb60ae692045d02473c1fd588fca330
Carter Harrison <carterjharrison@gmail.com>
2018-09-17 18:42:42 -0700

    pay return

---
+++ src/main/java/network/xyo/sdkcorekotlin/signing/algorithms/ecc/XyoEthAddress.kt
@@ -0,0 +1,27 @@
+package network.xyo.sdkcorekotlin.signing.algorithms.ecc
+
+import network.xyo.sdkcorekotlin.data.XyoObject
+import network.xyo.sdkcorekotlin.data.XyoObjectProvider
+
+class XyoEthAddress (val ethAddress: ByteArray) : XyoObject() {
+    override val id: ByteArray = byteArrayOf(major, minor)
+    override val objectInBytes: ByteArray = ethAddress
+
+    override val sizeIdentifierSize: Int?
+        get() = null
+
+    companion object : XyoObjectProvider() {
+        override val major: Byte = 0x02
+        override val minor: Byte = 0x0c
+
+        override val sizeOfBytesToGetSize: Int? = null
+
+        override fun createFromPacked(byteArray: ByteArray): XyoObject {
+            return XyoEthAddress(byteArray)
+        }
+
+        override fun readSize(byteArray: ByteArray): Int {
+            return 22
+        }
+    }
+}
