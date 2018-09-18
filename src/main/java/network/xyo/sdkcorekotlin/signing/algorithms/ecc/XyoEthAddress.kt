8b0fa0c03aa8cb071be480fe0d13f637af5929d2
Carter Harrison <carterjharrison@gmail.com>
2018-09-17 21:34:03 -0700

    ethereum address comment

--- src/main/java/network/xyo/sdkcorekotlin/signing/algorithms/ecc/XyoEthAddress.kt
+++ src/main/java/network/xyo/sdkcorekotlin/signing/algorithms/ecc/XyoEthAddress.kt
@@ -3,6 +3,11 @@ package network.xyo.sdkcorekotlin.signing.algorithms.ecc
 import network.xyo.sdkcorekotlin.data.XyoObject
 import network.xyo.sdkcorekotlin.data.XyoObjectProvider
 
+/**
+ * A Ethereum Address to be used when referencing ethereum accounts. For example in the XyoPayTo.
+ *
+ * @param ethAddress The Ethereum Address.
+ */
 class XyoEthAddress (val ethAddress: ByteArray) : XyoObject() {
     override val id: ByteArray = byteArrayOf(major, minor)
     override val objectInBytes: ByteArray = ethAddress
