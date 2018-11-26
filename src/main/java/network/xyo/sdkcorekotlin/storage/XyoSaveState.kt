8d4d2e3a26450062e2852178acd74805984cc063
Carter Harrison <carterjharrison@gmail.com>
2018-11-25 20:58:27 -0800

    change saveState function

--- src/main/java/network/xyo/sdkcorekotlin/storage/XyoSaveState.kt
+++ src/main/java/network/xyo/sdkcorekotlin/storage/XyoSaveState.kt
@@ -3,29 +3,44 @@ package network.xyo.sdkcorekotlin.storage
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import java.lang.Exception
 
 class XyoSaveState (private val storageProvider: XyoStorageProviderInterface) {
-    fun saveIndex (index : ByteArray) = GlobalScope.async {
-        storageProvider.write("index".toByteArray(), index).await()
+    fun saveIndex (index : ByteArray) : Deferred<Exception?> {
+        return writeFromKey(INDEX, index)
     }
 
-    fun saveSigners (privateKeys: ByteArray) = GlobalScope.async {
-        storageProvider.write("privateKeys".toByteArray(), privateKeys).await()
+    fun saveSigners (privateKeys: ByteArray) : Deferred<Exception?> {
+        return writeFromKey(SIGNERS_KEY, privateKeys)
     }
 
-    fun savePreviousHash (hash : ByteArray) = GlobalScope.async {
-        storageProvider.write("prevHash".toByteArray(), hash).await()
+    fun savePreviousHash (hash : ByteArray) : Deferred<Exception?>{
+        return writeFromKey(PREV_HASH_KEY, hash)
     }
 
-    fun getIndex () : Deferred<ByteArray?> = GlobalScope.async {
-        return@async storageProvider.read("index".toByteArray()).await()
+    fun getIndex () : Deferred<ByteArray?> {
+        return readFromKey(INDEX)
     }
 
-    fun getSigners () : Deferred<ByteArray?> = GlobalScope.async {
-        return@async storageProvider.read("privateKeys".toByteArray()).await()
+    fun getSigners () : Deferred<ByteArray?> {
+        return readFromKey(SIGNERS_KEY)
     }
 
-    fun getPreviousHash () : Deferred<ByteArray?> = GlobalScope.async {
-        return@async storageProvider.read("prevHash".toByteArray()).await()
+    fun getPreviousHash () : Deferred<ByteArray?> {
+        return readFromKey(PREV_HASH_KEY)
+    }
+
+    private fun readFromKey (key : String) = GlobalScope.async {
+        return@async storageProvider.read(key.toByteArray()).await()
+    }
+
+    private fun writeFromKey (key: String, value : ByteArray) = GlobalScope.async {
+        return@async storageProvider.write(key.toByteArray(), value).await()
+    }
+
+    companion object {
+        const val PREV_HASH_KEY = "prevHash"
+        const val SIGNERS_KEY = "privateKeys"
+        const val INDEX = "index"
     }
 }
