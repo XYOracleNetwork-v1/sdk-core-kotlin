82f116296949184322082f0448af6fab007f7534
Carter Harrison <carterjharrison@gmail.com>
2019-01-15 13:35:20 -0800

    change debug flag

--- src/main/kotlin/network/xyo/sdkcorekotlin/log/XyoAsciiLogger.kt
+++ src/main/kotlin/network/xyo/sdkcorekotlin/log/XyoAsciiLogger.kt
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object XyoAsciiLogger : XyoLogger {
-    private val isInDebug = System.getenv("debug") == "true"
+    private val isInDebug = System.getenv("DEBUG") == "true"
 
     private const val ANSI_RESET = "\u001B[0m"
     private const val ANSI_BLACK = "\u001B[30m"
--- src/main/kotlin/network/xyo/sdkcorekotlin/queries/XyoGetOriginBlockByHash.kt
+++ src/main/kotlin/network/xyo/sdkcorekotlin/queries/XyoGetOriginBlockByHash.kt
@@ -2,6 +2,7 @@ package network.xyo.sdkcorekotlin.queries
 
 import kotlinx.coroutines.Deferred
 import network.xyo.sdkcorekotlin.boundWitness.XyoBoundWitness
+import network.xyo.sdkcorekotlin.persist.XyoStorageException
 
 interface XyoGetOriginBlockByHash {
     /**
@@ -10,6 +11,8 @@ interface XyoGetOriginBlockByHash {
      * @param originBlockHash The previous hash of the origin block the function is looking
      * to find.
      * @return a deferred XyoPartyOriginBlock that is has the previous hash.
+     * @throws XyoStorageException if there is a problem reading
      */
+    @Throws(XyoStorageException::class)
     fun getOriginBlockByBlockHash (originBlockHash: ByteArray) : Deferred<XyoBoundWitness?>
 }
